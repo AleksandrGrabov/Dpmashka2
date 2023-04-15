056e58d23404b6d186620567deb0ae98ff3db1a3
AleksandrGrabov <grabov-it@yandex.by>
2023-04-15 19:51:49 +0300

    Нашли вторую цифру

---
+++ zadacha10/Program.cs
@@ -0,0 +1,10 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе
+//   показывает вторую цифру этого числа.
+
+System.Console.WriteLine("Введите число i: ");
+ int number = int.Parse(Console.ReadLine());
+
+int i = number % 100;
+int a = i / 10;
+
+System.Console.WriteLine($"Вторая цифра {a}");
